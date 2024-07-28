7c7047fc6dec3a88453d67c1de136b2ed69b477d
Mennatullah Fahmi <n.m.24xxx@gmail.com>
2024-07-28 09:10:24 +0000

    initiate

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import demographic_data_analyzer
 from unittest import main
 
-# Test your function by calling it here
+# Test your function by calling it here 
 demographic_data_analyzer.calculate_demographic_data()
 
 # Run unit tests automatically
